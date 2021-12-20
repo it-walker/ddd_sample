7b4852508df37047224c92ed70042dc542c7027e
Hiroki Sato <myhome.satoh+github@gmail.com>
2021-12-20 21:44:32 +0900

    change database sqlite3 -> mysql

--- task/src/app.module.ts
+++ task/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MemoModule } from './memo/memo.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    MemoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
